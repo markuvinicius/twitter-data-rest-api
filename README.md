acead1b0d22d3f63a586e06bdaf24ca795fcf49d
markuvinicius@gmail.com <markuvinicius@gmail.com>
2018-08-09 22:40:53 -0300

    fixing bugs

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# twitter-data-rest-api
-Restfull API para consumo dos dados do twitter coletados no projeto Twitter Data.
+# Twitter Data REST Api
+Restfull API feita em python para consumo dos dados do twitter coletados no projeto Twitter Data.
+
+## Features
+Oferece um grupo de endpoints para consumo dos dados ingeridos na base NoSql Cassandra pelo projeto Twitter Data.
+
+## EndPoints
+- **host:500/tags** Retorna uma estrutura de dados em formato json contendo a lista de tags coletadas pelo projeto, separadas e quantificadas por idioma 
+- **host:500/tags/<string:idioma>** Retorna uma estrutura de dados em formato json contendo a lista quantificada de tags coletadas pelo projeto, referente ao idioma informado (pt, en, es, fr, ...)
+- **host:500/users/<string:data>** Retorna uma estrutura de dados em formato json contendo lista dos 5 usuários com maior número de seguidores para a data informada.
+
+## Set-Up
+### Python
+Esta aplicação foi construída/testada utilizando a versão 2.7 do Python.<br>
+<a href=https://www.python.org/download/releases/2.7/>Instalação Python 2.7 </a>
+
+### PyPi
+A execução desta aplicação requer a instalação de pacotes e bibliotecas específicas. Recomenda-se o uso do `virtualenv` para segregar um ambiente virtual para execução e o pacote `pip` para a instalação dos pacotes.<br>
+Mais informações sobre a instalação do `virtualenv` estão aqui <a href=https://virtualenv.pypa.io/en/stable/installation/>aqui</a>
+
+- **Instalação com `virtualenv`-** Execute os seguintes comandos no terminal:<br>
+    - `virtualenv --python=Python2.7 $VENV_DIR/venv` <br>
+    Este comando criará um novo ambiente virtual Python chamado **venv** no diretório **$VENV_DIR**
+    - `source $VENV_DIR/venv/bin/activate` <br>
+    Este comando apontará o bash para o arquivo activate do ambiente virtual e carregará as bibliotecas necessárias para o ambiente.
+    - `pip install -r $APP_DIR/requirements.txt` <br>
+    Este comando instalará todas as dependências necessárias para a execução do projeto
+
+## Execução
+Para facilitar a execução do projeto, é fornecido um script shell na em `scripts/start_application.sh`. <br>
+A execução deverá ocorrer na porta padrão do Flask (host:5000)
--- app.py
+++ app.py
@@ -3,31 +3,30 @@ from flask_cqlalchemy import CQLAlchemy
 from flask import jsonify
 
 app = Flask(__name__)
-app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_HOSTS'] = ['localhost']
 app.config['CASSANDRA_KEYSPACE'] = "cz"
 db = CQLAlchemy(app)
 
-
 class Tweets_count_by_lang_tag(db.Model):
     language = db.columns.Text(required=True, primary_key=True)
-    tag = db.columns.Text(required=True,primary_key=True)
+    tag = db.columns.Text(required=True, primary_key=True)
     qtd = db.columns.BigInt(static=True)
 
 class Most_followed_users_by_process_date(db.Model):
     process_date = db.columns.Text(required=True, primary_key=True)
     followers_count = db.columns.BigInt(required=True, primary_key=True)
-    author_id = db.columns.BigInt(required=True,static=True)
-    friends_count = db.columns.BigInt(required=True,static=True)
-    tag = db.columns.Text(required=True,static=True)
-    username = db.columns.Text(required=True,static=True)
+    author_id = db.columns.BigInt(required=True, static=True)
+    friends_count = db.columns.BigInt(required=True, static=True)
+    tag = db.columns.Text(required=True, static=True)
+    username = db.columns.Text(required=True, static=True)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def landing_page():
+    return 'Requisite o endpoint desejado'
 
-@app.route('/users/most_followed/<string:date>', methods=["GET"])
+@app.route('/users/<string:date>', methods=["GET"])
 def get_most_followed_users_by_date(date):
-    users = Most_followed_users_by_process_date.objects(process_date=date).limit(10)
+    users = Most_followed_users_by_process_date.objects(process_date=date).limit(5)
 
     op_json = []
     for item in users:
@@ -40,7 +39,6 @@ def get_most_followed_users_by_date(date):
     resp.status_code = 200
 
     return resp
-
 
 @app.route("/tags", methods=["GET"])
 def get_all_tags():
@@ -61,7 +59,6 @@ def get_all_tags():
 
 @app.route("/tags/<string:lang>", methods=["GET"])
 def get_tags_by_language(lang):
-
     t = Tweets_count_by_lang_tag.objects(language=lang)
 
     op_json=[]
@@ -77,5 +74,6 @@ def get_tags_by_language(lang):
 
     return resp
 
+# main app execution
 if __name__ == '__main__':
     app.run()
